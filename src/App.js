e17d5aa27a6c6fc5de1be64b049aac521e7ad212
Abdoulaye Sanogo <asanogo919@gmail.com>
2025-09-03 21:28:11 +0100

    ajout du layout

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 // src/App.js
 
 import React from 'react';
-
-// Importations Amplify
 import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-
-// Importation de la configuration générée par Amplify
 import awsExports from './aws-exports';
 
-// Configuration d'Amplify pour qu'il se connecte à notre backend
+// On importe notre nouveau Layout
+import Layout from './components/Layout';
+
 Amplify.configure(awsExports);
 
-// Le composant App ne sera affiché QUE si l'utilisateur est connecté.
-// 'signOut' et 'user' sont fournis automatiquement par withAuthenticator.
 function App({ signOut, user }) {
   return (
-    <div>
-      <h1>
-        Bonjour, {user?.attributes?.email || "utilisateur"} !
-      </h1>
-      <p>Bienvenue sur le CMS Dobytrade.</p>
-      <button onClick={signOut}>Se déconnecter</button>
-    </div>
+    // On enveloppe notre contenu avec le Layout
+    // On lui passe les props 'signOut' et 'user' dont il a besoin
+    <Layout signOut={signOut} user={user}>
+      {/* Ce qui est ici est la prop "children" du Layout */}
+      <div>
+        <h1>Tableau de bord</h1>
+        <p>Bienvenue sur le CMS Dobytrade.</p>
+      </div>
+    </Layout>
   );
 }
 
-
-// withAuthenticator est un "Higher-Order Component" qui enveloppe notre App.
-// Il gère automatiquement l'affichage du formulaire de connexion/inscription
-// si l'utilisateur n'est pas authentifié.
 export default withAuthenticator(App);
